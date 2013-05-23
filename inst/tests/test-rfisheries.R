beb3a10027bdfff56f12243c4e523690925cd5b2
Karthik Ram <karthik.ram@gmail.com>
2013-05-23 10:52:24 -0700

    added tests

---
+++ inst/tests/test-rfisheries.R
@@ -0,0 +1,13 @@
+# tests for alm fxn in alm
+context("rfisheries")
+
+cc <- country_codes()
+sp <- species_codes()
+landings <- landings(country = 'CAN')
+
+test_that("Main functions return data.frames", {
+    expect_that(cc, is_a("data.frame"))
+    expect_that(sp, is_a("data.frame"))
+    expect_that(landings, is_a("data.frame"))
+})
+
