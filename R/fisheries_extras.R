6f7c9502898a38c964bbf01e768e4abbe0484e40
Karthik Ram <karthik.ram@gmail.com>
2013-11-28 17:08:46 -0800

    Plotting functions now retrive data from species and country datasets

--- R/fisheries_extras.R
+++ R/fisheries_extras.R
@@ -1,14 +1,19 @@
 #' Plots data for an rfisheries object
 #' 
-#'@importFrom ggplot2 ggplot
+#'@importFrom ggplot2 ggplot theme_get theme_update ggtitle geom_line labs theme_set element_line element_blank aes
 #' @param x A landings dataset belonging to either a species or a country.
+#' @param linecolor Default line color is steelblue
+#' @param linesize Default line size is 0.9
+#' @param title Plot title. Title is generated based on species or country code. Specify one here only if you need something else.
 #' @param ... additional arguments
 #' @export
 #' @examples \dontrun{
 #' fish_plot(landings(country = 'CAN'))
 #'}
-fish_plot <- function(x, ...) {
+fish_plot <- function(x, linecolor = "steelblue", linesize = 0.9, title = NULL, ...) {
 
+year <- NA 
+catch <- NA
 # Both datasets really should have 3 columns.
 # Otherwise something is wrong
 stopifnot(class(x) == "data.frame")
@@ -27,21 +32,24 @@ theme_update(panel.background = element_blank(), panel.grid.major = element_blan
         axis.line = element_line(colour = "black"))
 
 if(identical(species_dataset, names(x))) {
-if(is.null(title)) {    
-    title <- paste0("Landings for ", unique(x$species))    
+if(is.null(title)) {
+    data(species_code_data)
+    english_name <- species_code_data[grep(unique(x$species),species_code_data),]$english_name
+    title <- paste0("Landings for ", english_name, "(", unique(x$species), ")")    
 }
 fish_plot <- ggplot(x, aes(year, catch)) + 
-geom_line(color = "steelblue", size = .9) + 
+geom_line(color = linecolor, size = linesize) + 
 labs(x= "Year", y= "Catch (in tonnes)") + 
 ggtitle(title)
 }
 
 if(identical(country_dataset, names(x))) {
-    if(is.null(title)) {    
+    if(is.null(title)) {
+        # [Todo]: Add species or country name into the title.
     title <- paste0("Landings for ", unique(x$country))    
     }
 fish_plot <-  ggplot(x, aes(year, catch)) + 
-geom_line(color = "steelblue", size = .9) + 
+geom_line(color = linecolor, size = linesize) + 
 labs(x= "Year", y= "Catch (in tonnes)") + 
 ggtitle(title)
 }
